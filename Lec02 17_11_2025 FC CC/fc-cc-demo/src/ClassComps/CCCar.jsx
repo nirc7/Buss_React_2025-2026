9f031e2d2ba698e64e569de8cef6d69cf17998d1
nc <31476486+nirc7@users.noreply.github.com>
2025-11-17 14:47:30 +0200

    fc cc

---
+++ Lec02 17_11_2025 FC CC/fc-cc-demo/src/ClassComps/CCCar.jsx
@@ -0,0 +1,46 @@
+import { Component } from "react";
+
+export default class CCCar extends Component {
+  constructor(props) {
+    super(props);
+
+    let num = 7;
+    this.age = 50;
+    this.speed = this.props.speed + 5;
+
+    this.state = {
+      speed: this.props.speed + 7,
+      color:'red'
+    };
+
+  }
+
+  btnAddSpeed = () => {
+    // console.log(this.speed);
+    // this.speed += 5;
+    // console.log(this.speed);
+
+    //this.state.speed = 500; //ERROR!
+    console.log(this.state.speed);
+    this.setState({ speed: 500 }); 
+    console.log(this.state.speed);
+    //code...
+  }
+
+
+  render() {
+
+    console.log( 'render' ,  this.state.speed);
+
+
+    return (
+      <div>
+        <h3>Car</h3>
+        model = {this.props.model} <br />
+        speed = {this.state.speed} <br />
+        {this.age} <br />
+        <button onClick={this.btnAddSpeed}>Accelerate</button>
+      </div>
+    );
+  }
+}
